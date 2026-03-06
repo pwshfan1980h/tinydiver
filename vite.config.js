63dbd08197b270388002488c7d74878612144bc5
Will Markham <devwm8@gmail.com>
2026-03-06 11:32:57 -0500

    Add GitHub Pages deployment pipeline

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
+const pagesBasePath = process.env.PAGES_BASE_PATH || '/';
+
 export default defineConfig({
+  base: pagesBasePath,
   server: {
     port: 3000,
     open: true
